0cfc10694f45ddb3c8630db97dd73d50adbc0d25
leowg <leowg@07ddbf37-e709-494b-991b-d5d4a0e70382>
2014-06-26 03:36:24 +0000

    update to swift mailer

--- equiz/app/emailer.php
+++ equiz/app/emailer.php
@@ -11,41 +11,64 @@
 *
 * email helper class
 * emailer.php 2010-09-20 leow
+* 2014-06-25	leow	introduce swift mailer
 */
+require_once('swift_required.php');
+
 Class EmailHelper {
-	protected $args = array('to' => null,
-	                        'from' => null,
-	                        'subject' => "EmailHelper test",
-	                        'message' => "EmailHelper test",
-	                        'headers' => "MIME-Version: 1.0\r\nContent-type: text/html; charset=UTF-8\r\n",
-	                        'cc' => null,
-	                        'bcc' => null);
+	protected $mailer;
 
-	public function EmailHelper($args=array()) {
-		$this->update($args);
+	public function EmailHelper() {
+		global $smtpSvr, $smtpPort;
+		$transport = Swift_SmtpTransport::newInstance($smtpSvr, $smtpPort);
+		// Create the Mailer using your created Transport
+		$this->mailer = Swift_Mailer::newInstance($transport);
 	}
 
-	public function update($args=array()) {
-		if (!is_array($args)) {return;}
-		foreach (array_keys($this->args) as $k) {
-			if(isset($args[$k])) {$this->args[$k] = $args[$k];}
+	public function composeMsg($args=array()) {
+		global $dbgEmail;
+		$message = Swift_Message::newInstance();
+		// Give the message a subject
+		if(isset($args['subject'])) {
+			$message->setSubject($args['subject']);
+		} else {
+			$message->setSubject('Test message');
 		}
+
+		// Set the From address with an associative array
+		if(isset($args['from'])) {
+			$message->setFrom($args['from']);
+		} else {
+			$message->setFrom($dbgEmail);
+		}
+
+		// Set the To addresses with an associative array
+		if(isset($args['to'])) {
+			$message->setTo($args['to']);
+		} else {
+			$message->setTo($dbgEmail);
+		}
+
+		// Give it a body
+		//  ->setBody('Here is the message itself语文。', 'text/html')
+
+		// And optionally an alternative body
+		//->addPart('<q>Here is the message itself友谊</q>', 'text/html')
+
+		// Optionally add any attachments
+		//->attach(Swift_Attachment::fromPath('my-document.pdf'))
+  		//;
+ 		// print the msg as text for debug purpose
+		//echo $message->toString(); 
+		return $message;
 	}
 
-	public function sndMail($args=array()) {
-		if (!is_array($args)) {return False;}
-		foreach (array_keys($this->args) as $k) {
-			if(!isset($args[$k])) {$args[$k] = $this->args[$k];}
-		}
-		$headers = $args['headers']."From: ".$args['from']."\r\n";
-		$headers = $args['headers']."Reply-To: ".$args['from']."\r\n";
-		$headers = $args['headers']."Return-Path: ".$args['from']."\r\n";
+	public function sndMail($message) {
 		//options to send to cc+bcc
 		if(isset($args['cc'])) { $headers .= "Cc: ".$args['cc']."\r\n"; }
 		if(isset($args['bcc'])) { $headers .= "Bcc: ".$args['bcc']."\r\n"; }
-		// now lets send the email.
 		try {
-			mail($args['to'], $args['subject'], $args['message'], $headers);
+			$result = $this->mailer->send($message);
 		}
 		catch (Exception $e) {
 			echo "send mail Failed:\n" . $e->getMessage();
